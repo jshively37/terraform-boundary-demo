1127b371207499589401f6436d9dcb9fbe0e9119
Jeffrey <jeffrey.shively@hashicorp.com>
2023-01-31 13:46:06 -0500

    Validated Boundary issues creds from Vault to Postgres docker container

---
+++ configure_hcp_boundary/configure_boundary_host.tf
@@ -0,0 +1,27 @@
+resource "boundary_host_static" "host" {
+  name            = var.boundary_static_host_name
+  description     = var.boundary_static_host_desc
+  address         = var.boundary_static_host_addr
+  host_catalog_id = boundary_host_catalog_static.catalog.id
+}
+
+resource "boundary_host_set_static" "host" {
+  host_catalog_id = boundary_host_catalog_static.catalog.id
+  host_ids = [
+    boundary_host_static.host.id,
+  ]
+}
+
+resource "boundary_target" "host" {
+  name         = var.boundary_target_host_name
+  description  = var.boundary_target_host_desc
+  type         = "tcp"
+  default_port = var.boundary_target_host_port
+  scope_id     = boundary_scope.project.id
+  host_source_ids = [
+    boundary_host_set_static.host.id
+  ]
+  brokered_credential_source_ids = [
+    boundary_credential_library_vault.cred_library.id
+  ]
+}
